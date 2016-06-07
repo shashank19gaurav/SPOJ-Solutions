d283640e1343b48f54e535e7af603e49ea27a59a
Shashank Gaurav <shashank19gaurav@gmail.com>
2016-06-07 08:53:22 +0530

    Improved FACEFRND

--- FACEFRND.cpp
+++ FACEFRND.cpp
@@ -1,41 +1,27 @@
+#include <stdio.h>
 #include <iostream>
 #include <set>
+
 using namespace std;
 
 int main() {
+    short nTestCases, nFriends;
+    scanf("%hd", &nTestCases);
+    set<short> friendsAndFriendsofFriend;
 
-	set<int> friendsOfFriends;
-	set<int> friends;
-	
-	int nTestCases;
-	int currentFriend, nCurrentFriendOfFriend, currentFriendOfFriend;
-	scanf("%d", &nTestCases);
-	
-	while(nTestCases--){
-		scanf("%d", &currentFriend);
-		friends.insert(currentFriend);
-		if(friendsOfFriends.find(currentFriend)!= friendsOfFriends.end()){
-			//This friend is present in friend of friend list
-			//Delete it
-			
-			friendsOfFriends.erase(currentFriend);
-		}
-		scanf("%d", &nCurrentFriendOfFriend);
-		while(nCurrentFriendOfFriend--){
-			scanf("%d", &currentFriendOfFriend);
-			if(friendsOfFriends.find(currentFriendOfFriend) == friendsOfFriends.end()){
-				//This Friend of Friend not in the list
-				//Insert it
-				if(friends.find(currentFriendOfFriend) == friends.end()) {
-					friendsOfFriends.insert(currentFriendOfFriend);
-				}
-				
-			}
-		}
-	}
-	
-	
-	cout<<friendsOfFriends.size();
+    short bobFriend, temp;
 
-	return 0;
+    for (int i = 0; i < nTestCases ; i++) {
+        scanf("%hd", &bobFriend);
+        friendsAndFriendsofFriend.insert(bobFriend);
+        scanf("%hd", &nFriends);
+        while(nFriends--) {
+            scanf("%hd", &temp);
+            friendsAndFriendsofFriend.insert(temp);
+        }
+    }
+
+    printf("%d", friendsAndFriendsofFriend.size() - nTestCases);
+
+    return 0;
 }
